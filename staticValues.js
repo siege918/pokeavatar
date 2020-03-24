5ec8dfdb43f2a9627d02a10ce4db74ffba2be09d
BuildTools <git@collin.fun>
2020-03-24 16:25:40 -0700

    Update pokes

--- staticValues.js
+++ staticValues.js
@@ -1,14 +1,16 @@
 module.exports = {
     // Alakazam = Ninetales
     "295": 37,
-    // Danny = Smeargle
-    "2749": 234,
+    // Danny = flaafy
+    "2749": 179,
     // Nills = ?
     "3802": 253,
     // Zondekel = Oddish
     "4258": 42,
     // Masked Hero = Voltorb
     "5961": 100,
-    // qwerty = dewgong
-    "7602": 86,
+    // qwerty = pidgey
+    "7602": 15,
+    // pastiche = haunter
+    "9185": 92
 };
