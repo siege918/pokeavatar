047b07c796f20d90f8736e56c7c26723be9e92f6
BuildTools <git@collin.fun>
2020-03-28 16:20:19 -0700

    Remove identifying comments

--- staticValues.js
+++ staticValues.js
@@ -1,16 +1,10 @@
 module.exports = {
     "27": 79,
-    // Alakazam = Ninetales
     "295": 37,
-    // Danny = flaafy
     "2749": 179,
-    // Nills = ?
     "3802": 253,
-    // Zondekel = Oddish
     "4258": 42,
-    // Masked Hero = Voltorb
     "5961": 100,
-    // qwerty = pidgey
     "7602": 16,
     "9185": 92,
 };
